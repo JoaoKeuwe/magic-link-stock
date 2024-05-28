2ed27f8903310a434907a45d2e8acff105125935
joaokeuwe <joaov2872@hotmail.com>
2024-05-28 12:21:32 -0300

    feat:

--- components/html/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Email from "../../emails/index";
-import { render } from "@react-email/render";
-
-const html = render(<Email />, {
-  pretty: true,
-});
-
-console.log('teste' ,html);
